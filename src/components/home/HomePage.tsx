f7005191e3e87122ab017073d3db7ce00ec93b0a
Yahor_Hlushak@epam.com <Rtyytregor9244264>
2020-08-28 14:16:48 +0300

    task-3-4: added useMemo for popups rendering

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import Header from './header/Header';
 import Main from './main/Main';
 import Footer from './footer/Footer';
 import { useSelector } from 'react-redux';
-import { IMoviePopupsState } from '../stores/reducers/MoviePopupsReducer';
+import { IMoviePopupsState } from '../../stores/reducers/MoviePopupsReducer';
 import AddMoviePopup from '../popups/moviePopup/addMoviePopup/AddMoviePopup';
 import EditMoviePopup from '../popups/moviePopup/editMoviePopup/EditMoviePopup';
 import DeleteMoviePopup from '../popups/moviePopup/deleteMoviePopup/DeleteMoviePopup';
@@ -13,18 +13,18 @@ import DeleteMoviePopup from '../popups/moviePopup/deleteMoviePopup/DeleteMovieP
 const HomePage: React.FC = () => {
     const isPopupOpened = useSelector<IMoviePopupsState, IMoviePopupsState["isPopupOpened"]>(
         state => state.isPopupOpened
-    )
+    );
     const popupName = useSelector<IMoviePopupsState, IMoviePopupsState["popupName"]>(
         state => state.popupName
-    )
+    );
     const popupTitle = useSelector<IMoviePopupsState, IMoviePopupsState["popupTitle"]>(
         state => state.popupTitle
-    )
+    );
     const popupData = useSelector<IMoviePopupsState, IMoviePopupsState["movie"]>(
         state => state.movie
-    )
+    );
 
-    function getActivePopup() {
+    const activePopup = useMemo(() => {
         switch (popupName) {
             case 'addMoviePopup':
                 return <AddMoviePopup title={popupTitle}/>;
@@ -33,9 +33,9 @@ const HomePage: React.FC = () => {
             case 'deleteMoviePopup':
                 return <DeleteMoviePopup title={popupTitle} movie={popupData}/>;
             default:
-                return ''
+                return;
         }
-    }
+    }, [isPopupOpened]);
 
     return (
         <>
@@ -44,7 +44,7 @@ const HomePage: React.FC = () => {
                 <Main/>
                 <Footer/>
             </div>
-            {isPopupOpened && getActivePopup()}
+            {activePopup}
         </>
 
     );
